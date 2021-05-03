585371487260e6e48cd57a690c723ebdb7543cb6
Anastasia Burdina <naste-f@yandex.ru>
2021-05-03 12:09:26 +0300

    feat: change folders

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model, Types} = require('mongoose')
-
-const schema = new Schema({
-  email: {type: String,  require: true, unique: true},
-  password: {type: String,  require: true},
-  links: [{ type: Types.ObjectId, ref: "Link"}]
-})
-
-module.exports = model( 'User', schema)
